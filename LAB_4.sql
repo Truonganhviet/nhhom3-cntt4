55ca39a1a061c6474b86f33c6b8f11b6c2b19e95
truonganhviet <118434726+Truonganhviet@users.noreply.github.com>
2022-11-24 23:43:20 +0700

    Update LAB_4.sql

--- LAB_4.sql
+++ LAB_4.sql
@@ -78,3 +78,32 @@ BEGIN
 SET @A=N' KHONG HOP LE'
 RAISERROR (@A,16,1)
 END
+
+DECLARE @i INT = 0, @tong INT = 0;
+WHILE @i <= 10
+	BEGIN 
+		PRINT @i; 
+			IF(@i%2 = 0)
+				BEGIN
+					SET @tong = @tong + @i;
+				END;
+		SET @i = @i + 1;
+	END;
+	PRINT 'tong la: ' + str(@tong);
+GO
+
+DECLARE @i INT = 0, @tong INT = 0;
+WHILE @i <= 10
+	BEGIN 
+		PRINT @i; 
+			IF(@i%2 = 0)
+				BEGIN
+				IF(@i != 4)
+					BEGIN
+						SET @tong = @tong + @i;
+					END;
+				END;
+		SET @i = @i + 1;
+	END;
+	PRINT 'tong la: ' + str(@tong);
+GO
